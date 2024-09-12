cb7b0948356357667ad608e4b6c26f11a1d31d37
sunmok <tjsahr9191@naver.com>
2024-09-12 16:07:41 +0900

    feat : 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.config.GameConfig;
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameConfig gameConfig = new GameConfig(
+                new InputView(),
+                new OutputView()
+        );
+
+        GameController gameController = new GameController(gameConfig);
+        gameController.run();
     }
 }
