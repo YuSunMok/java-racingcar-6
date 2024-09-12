48b8bcc97ca2f6e22a2298cde7bcc7a158813aa3
sunmok <tjsahr9191@naver.com>
2024-09-12 16:10:08 +0900

    feat : 랜덤 넘버 생성

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.config.GameConfig;
+
+public class RandomNumberGenerator {
+
+    public static int generate() {
+        return Randoms.pickNumberInRange(GameConfig.START_NUMBER_FOR_RANDOM_NUMBER, GameConfig.END_NUMBER_FOR_RANDOM_NUMBER);
+    }
+}
